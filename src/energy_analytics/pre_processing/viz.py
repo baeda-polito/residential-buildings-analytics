5c713b2f236d7ecffa099ee06806e21824eec844
Giudice7 <rocco.giudice7@gmail.com>
2025-01-16 11:40:30 +1100

    feat(energy_analytics): visualizzazione pre-processing

--- src/energy_analytics/pre_processing/viz.py
+++ src/energy_analytics/pre_processing/viz.py
@@ -3,9 +3,14 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap, Normalize
+import plotly.graph_objects as go
+import plotly.io as pio
 
 from settings import PROJECT_ROOT
 from ..aggregate import Aggregate
+from ..building import Building
+
+pio.renderers.default = "browser"
 
 
 def plot_heatmap_nan(aggregate: Aggregate, date_start: str, date_end: str) -> None:
@@ -107,3 +112,102 @@ def plot_heatmap_nan(aggregate: Aggregate, date_start: str, date_end: str) -> No
 
     plt.tight_layout()
     plt.savefig(os.path.join(PROJECT_ROOT, "figures", "pre_processing", f"missing_values_production_{aggregate.name}.png"), dpi=300)
+
+
+def plot_pre_processing(building: Building):
+    """
+    Crea un grafico con la potenza grezza e pulita per un edificio e lo salva in un file HTML. Se l'edificio è un
+    produttore, crea anche un grafico con la produzione grezza e pulita. Salva i file nella cartella "figures/pre_processing".
+
+    Args:
+        building (Building): Oggetto Building con le informazioni sull'edificio.
+
+    Returns:
+        None
+    """
+
+    data_raw = building.energy_data.data_raw
+    data_cleaned = building.energy_data.data_cleaned
+
+    trace1 = go.Scatter(
+        x=data_raw["timestamp"],
+        y=data_raw['power'],
+        mode='lines',
+        name='power raw',
+        line=dict(width=2, color='#b6353f'),
+        hovertemplate='Data: %{x}<br>Potenza: %{y:.2f} W<extra></extra>'
+    )
+
+    trace2 = go.Scatter(
+        x=data_cleaned["timestamp"],
+        y=data_cleaned['power'],
+        mode='lines',
+        name='power cleaned',
+        line=dict(width=1.2, color='#2bb06a'),
+        hovertemplate='Data: %{x}<br>Potenza: %{y:.2f} W<extra></extra>'
+    )
+
+    trace3 = go.Scatter(
+        x=data_raw["timestamp"],
+        y=data_raw['productionPower'],
+        mode='lines',
+        name='productionPower raw',
+        line=dict(width=2, color='#b6353f'),
+        hovertemplate='Data: %{x}<br>Potenza: %{y:.2f} W<extra></extra>'
+    )
+
+    trace4 = go.Scatter(
+        x=data_cleaned["timestamp"],
+        y=data_cleaned['productionPower'],
+        mode='lines',
+        name='productionPower cleaned',
+        line=dict(width=1.2, color='#2bb06a', dash='dash'),
+        hovertemplate='Data: %{x}<br>Potenza: %{y:.2f} W<extra></extra>'
+    )
+
+    # Create the layout with legend positioned at the top center
+    layout = go.Layout(
+        title=dict(
+            text=f"Pre-processing per {building.building_info['name']}",
+            x=0.5,
+            font=dict(size=20, color='black')
+        ),
+        font=dict(family='Poppins'),
+        xaxis=dict(
+            title='Data e ora',
+            showgrid=True,
+            gridcolor='#dfdfdf',
+            gridwidth=0.2,
+            zeroline=True,
+            zerolinecolor='black',
+            zerolinewidth=1,
+            tickfont=dict(size=14),
+            titlefont=dict(size=18)
+        ),
+        yaxis=dict(
+            title='Potenza [W]',
+            showgrid=True,
+            gridcolor='#dfdfdf',
+            gridwidth=0.2,
+            tickfont=dict(size=14),
+            titlefont=dict(size=18)
+        ),
+        paper_bgcolor='white',
+        plot_bgcolor='white',
+        showlegend=True,
+        legend=dict(
+            orientation='h',
+            yanchor='bottom',
+            y=0.97,
+            xanchor='center',
+            x=0.5,
+            font=dict(size=16, color='black')
+        )
+    )
+
+    fig_power = go.Figure(data=[trace2, trace1], layout=layout)
+    pio.write_html(fig_power, os.path.join(PROJECT_ROOT, "figures", "pre_processing", f"{building.building_info['name']}_power_pre_processing.html"))
+
+    if building.building_info['user_type'] != "consumer":
+        fig_pv = go.Figure(data=[trace4, trace3], layout=layout)
+        pio.write_html(fig_pv, os.path.join(PROJECT_ROOT, "figures", "pre_processing", f"{building.building_info['name']}_productionPower_pre_processing.html"))
